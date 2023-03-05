31c7a9cfb66ed5ec89ffb88ecf40e9bd27999d37
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2023-03-05 18:03:07 +0100

    fix(generator): use the new chat completion API

--- generator.py
+++ generator.py
@@ -3,17 +3,35 @@ import openai
 
 MODEL = "gpt-3.5-turbo"
 
+SYS_ID = 'sysId'
+
+APIKEY = 'key'
+ORG = 'org'
+
 def generate_ino(prompt: str):
-    openai.Completion.create(
+    import json
+    f_conf = open('config.json')
+    config = json.load(f_conf)
+    sysId = config[SYS_ID]
+
+    userPrompt = {
+        "role": "user",
+        "content": prompt[0]
+    }
+
+    return openai.ChatCompletion.create(
         model=MODEL,
-        prompt=prompt
+        messages=[
+            sysId,
+            userPrompt
+        ]
     )
 
 if __name__ == "__main__":
     import json
     f = open('token.json')
     data = json.load(f)
-    openai.api_key = data['key']
+    openai.api_key = data[APIKEY]
     openai.organization = data['org']
     prompt = sys.argv[1:]
     print("introducing: " + prompt[0])

662ead981c64d50f1fa232cbf288aeb4857d8b44
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2023-03-05 15:40:04 +0100

    feat(generator): create a generator that introduces the prompt to the OpenAI API

---
+++ generator.py
@@ -0,0 +1,19 @@
+import sys
+import openai
+
+MODEL = "gpt-3.5-turbo"
+
+def generate_ino(prompt: str):
+    openai.Completion.create(
+        model=MODEL,
+        prompt=prompt
+    )
+
+if __name__ == "__main__":
+    import json
+    f = open('token.json')
+    data = json.load(f)
+    openai.api_key = data['key']
+    prompt = sys.argv[1:]
+    print("introducing: " + prompt[0])
+    print(generate_ino(prompt=prompt))
